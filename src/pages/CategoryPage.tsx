682514d4756b6d5fa6ec8d5dfdf0255ce982e9f8
Ivan Soltynchuk <isoltynchuk@gmail.com>
2021-12-06 18:36:38 +0200

    Bind categoryQuery to CategoryPage

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,7 +1,27 @@
+import { gql } from "@apollo/client";
+import { graphql } from "@apollo/client/react/hoc";
 import React from "react";
 import { Link } from "react-router-dom";
 
-type Props = {};
+type Product = {
+  id: number;
+  name: string;
+};
+
+type Category = {
+  name: string;
+  products: Product[];
+};
+
+type CategoryQueryResult = {
+  loading: boolean;
+  category: Category;
+};
+
+type Props = {
+  data: CategoryQueryResult;
+};
+
 type State = {};
 
 class CategoryPage extends React.Component<Props, State> {
@@ -11,13 +31,33 @@ class CategoryPage extends React.Component<Props, State> {
     this.state = {};
   }
   render() {
+    const { loading, category } = this.props.data;
+    if (loading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
         <h1>CategoryPage</h1>
-        <Link to="/product/10">Product 10</Link>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {category.products.map((product) => {
+            return <Link to={`/product/${product.id}`}>{product.name}</Link>;
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default CategoryPage;
+const categoryQuery = gql`
+  query {
+    category(input: { title: "tech" }) {
+      name
+      products {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export default (graphql(categoryQuery as any) as any)(CategoryPage);
